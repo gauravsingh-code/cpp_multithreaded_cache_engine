d23807ebc92ed0b483618e0f8f94a8fbd07ad070
gauravkushwah284 <gaurav.kushwah@silvertouch.com>
2026-07-10 20:34:49 +0530

    added client feture

--- src/main.cpp
+++ src/main.cpp
@@ -6,95 +6,95 @@
 
 
 //-------Checking for the TcpServer class----------------
-// int main(){
-
-//     TcpServer server(8080);
-//     server.start();
-
-//     return 0;
-// }
-
 int main(){
 
-    std::cout << "******** NEW BUILD ********\n";
-    CacheEngine cache;
-    CommandParser parser;
-
-    std::string input;
-
-    while(true){
-        std::cout << "> ";
-
-        std::getline(std::cin, input);
-        if (!input.empty() && input.back() == '\r') input.pop_back(); // strip Windows \r
-
-        auto command = parser.parse(input);
-
-        switch(command.type){
-            case CommandType::SET:
-            {
-                cache.set(command.key, command.value);
-                std::cout<<"Key: "<<command.key<<" Value: "<<command.value<<"\n";
-                std::cout<<"OK\n";
-                break;
-            }
-            case CommandType::GET:{
-                auto value = cache.get(command.key);
-                if(value){
-                    std::cout<<*value<<'\n'; 
-                }
-                else{
-                    std::cout<<"NOT FOUND\n";
-                }
-                break;
-            }
-
-            case CommandType::DELETE_KEY:{
-                bool removed = cache.erase(command.key);
-                std::cout<<(removed ? "OK" : "NOT FOUND")<<"\n";
-                break;
-            }
-            
-            case CommandType::EXIT:{
-                return 0;
-            }
-
-            case CommandType::ALL:{
-                cache.all();
-                break;
-            }
-
-            default:
-            {
-                std::cout<<"INVALID COMMAND\n";
-                std::cout<<"INPut: "<<input<<"\n";
-            }
-
-        }
-
-    }
-
-
-
-    // CacheEngine cache;
-
-    // cache.set("name", "raghav");
-    // cache.set("city", "ahmedabad");
-
-    // auto name = cache.get("city");
-
-    // if(name){
-    //     std::cout<<*name<<"\n";
-    // }
-
-    // cache.erase("city");
-
-    //  auto city = cache.get("city");
-
-    // if(!city)
-    // {
-    //     std::cout << "Not Found\n";
-    // }
+    TcpServer server(8080);
+    server.start();
 
     return 0;
 }
+
+// int main(){
+
+//     std::cout << "******** NEW BUILD ********\n";
+//     CacheEngine cache;
+//     CommandParser parser;
+
+//     std::string input;
+
+//     while(true){
+//         std::cout << "> ";
+
+//         std::getline(std::cin, input);
+//         if (!input.empty() && input.back() == '\r') input.pop_back(); // strip Windows \r
+
+//         auto command = parser.parse(input);
+
+//         switch(command.type){
+//             case CommandType::SET:
+//             {
+//                 cache.set(command.key, command.value);
+//                 std::cout<<"Key: "<<command.key<<" Value: "<<command.value<<"\n";
+//                 std::cout<<"OK\n";
+//                 break;
+//             }
+//             case CommandType::GET:{
+//                 auto value = cache.get(command.key);
+//                 if(value){
+//                     std::cout<<*value<<'\n'; 
+//                 }
+//                 else{
+//                     std::cout<<"NOT FOUND\n";
+//                 }
+//                 break;
+//             }
+
+//             case CommandType::DELETE_KEY:{
+//                 bool removed = cache.erase(command.key);
+//                 std::cout<<(removed ? "OK" : "NOT FOUND")<<"\n";
+//                 break;
+//             }
+            
+//             case CommandType::EXIT:{
+//                 return 0;
+//             }
+
+//             case CommandType::ALL:{
+//                 cache.all();
+//                 break;
+//             }
+
+//             default:
+//             {
+//                 std::cout<<"INVALID COMMAND\n";
+//                 std::cout<<"INPut: "<<input<<"\n";
+//             }
+
+//         }
+
+//     }
+
+
+
+//     // CacheEngine cache;
+
+//     // cache.set("name", "raghav");
+//     // cache.set("city", "ahmedabad");
+
+//     // auto name = cache.get("city");
+
+//     // if(name){
+//     //     std::cout<<*name<<"\n";
+//     // }
+
+//     // cache.erase("city");
+
+//     //  auto city = cache.get("city");
+
+//     // if(!city)
+//     // {
+//     //     std::cout << "Not Found\n";
+//     // }
+
+//     return 0;
+// }
--- src/network/TcpServer.cpp
+++ src/network/TcpServer.cpp
@@ -24,6 +24,7 @@ bool TcpServer::start(){
     //this is gonna actually returna a socket 
     // A gate through other devices can connect to our device
     //it decided by the windows whether we get the socket or not
+    //Address family, type of socket, protocol
     listenSocket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);  
 
     //Socket is just like the phone without the sim 
@@ -33,7 +34,39 @@ bool TcpServer::start(){
         return false;
     }
 
-    std::cout<<"Created Socket Successfullly .\n";    
+    sockaddr_in serverAddress{};
+
+    serverAddress.sin_family = AF_INET; //IPv4
+    serverAddress.sin_addr.s_addr = INADDR_ANY; //bind to all available interfaces
+    serverAddress.sin_port = htons(port_); //convert port to network byte order
+
+    if(bind(listenSocket_, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR){
+        std::cout<<"Bind Failed with error: "<<WSAGetLastError()<<std::endl;
+        closesocket(listenSocket_);
+        WSACleanup();
+        return false;
+    }
+
+    if(listen(listenSocket_, SOMAXCONN) == SOCKET_ERROR){
+        std::cout<<"Listen Failed with error: "<<WSAGetLastError()<<std::endl;
+        closesocket(listenSocket_);
+        WSACleanup();
+        return false;
+    }
+
+    std::cout<<"Waiting for a client to connect on port "<<port_<<"...\n";
+
+    clientSocket_ = accept(listenSocket_, nullptr, nullptr);  //accept the connection from the client
+
+    if(clientSocket_ == INVALID_SOCKET){
+        std::cout<<"Failed to accept the connection from the client\n";
+        // closesocket(listenSocket_);
+        // WSACleanup();
+        return false;
+    }
+
+
+    std::cout<<"Client Connected successfully .\n";    
     return true;
 }
 
